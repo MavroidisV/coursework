7f8ca92e54bbbaad9122933e54e7893c7508bf9c
mavro <mavroidis@hotmail.gr>
2016-03-22 11:38:13 +0000

    coursework

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'bbe82b02f4e2d3');
+define('DB_PASSWORD', 'ed3bcdef');
+define('DB_DATABASE', 's_1510896');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
