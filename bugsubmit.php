2347516d936a4aee4a29d0d183fb71973aad1f19
mavro <mavroidis@hotmail.gr>
2016-03-23 15:02:38 +0000

    coursework

--- bugsubmit.php
+++ bugsubmit.php
@@ -2,18 +2,19 @@
 session_start();
 include("connection.php");
 include("check.php");
+$id=$_SESSION["userID"];
 //check if it is set; if the user has typed the requirements in the fields
 //in the html we have asked for required fields in first name and last name, but not in php; so with a proxy they can be bypassed;
 if(isset($_POST["submit"])) {
     $title = $_POST["title"];
     $description = $_POST["description"];
-    $id_check;
+   
 
     $title = mysqli_real_escape_string($db, $title);
     $description = mysqli_real_escape_string($db, $description);
 
 
-    $query = mysqli_query($db, "INSERT INTO bugs (title,description,userID)VALUES ('$title','$description','$id_check')") or die(mysqli_error($db));
+    $query = mysqli_query($db, "INSERT INTO bugs (title,description,userID)VALUES ('$title','$description','$id')") or die(mysqli_error($db));
     if (($query) == 1) {
         echo "Thank You! you are now registered.";
         echo "Please <a href = userlogin.php>Login</a>";
