224b33d3a4eb8e966c339f2d2b6444b3575a5d65
mavro <mavroidis@hotmail.gr>
2016-03-23 10:21:39 +0000

    coursework

---
+++ regsubmit.php
@@ -0,0 +1,36 @@
+<?php
+//check if it is set; if the user has typed the requirements in the fields
+//in the html we have asked for required fields in first name and last name, but not in php; so with a proxy they can be bypassed;
+if(isset($_POST["submit"]))
+{
+    $firstname1= $_POST["firstname"];
+    $lastname1 = $_POST["lastname"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $name = mysqli_real_escape_string($db, $username);
+    $email = mysqli_real_escape_string($db, $email);
+    $password = mysqli_real_escape_string($db, $password);
+    $password = md5($password);
+
+
+    $sql="SELECT email FROM users WHERE email='$email'";
+    $result=mysqli_query($db,$sql);
+    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+    if(mysqli_num_rows($result) == 1)
+    {
+        $msg = "Sorry...This email already exists...";
+    }
+    else
+    {
+        //echo $name." ".$email." ".$password;
+        $query = mysqli_query($db, "INSERT INTO users (username, email, password)VALUES ('$name', '$email', '$password')")or die(mysqli_error($db));
+        if($query)
+        {
+            $msg = "Thank You! you are now registered.";
+        }
+
+    }
+}
+?>
