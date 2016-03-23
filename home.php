90d16e9cde928bf7e8185ba4035a80f74679ec5b
mavro <mavroidis@hotmail.gr>
2016-03-23 08:56:25 +0000

    coursework

--- home.php
+++ home.php
@@ -6,8 +6,9 @@
 </head>
 
 <body>
-<h1>Hello</h1>
-<?php echo "Hello" .$_POST['username']?>
+<h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
+<br><br><br>
+<a href="logout.php" style="font-size:18px">Logout?</a>
 </body>
 </html>
  
