e87d29185c179e4777de37cf60ff6efd7f734590
mavro <mavroidis@hotmail.gr>
2016-03-23 12:28:58 +0000

    coursework

--- home.php
+++ home.php
@@ -12,6 +12,8 @@ include("check.php");
 
 <body>
 <h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
+
+<a href = bugform.php>submit a form</a>
 <br><br><br>
 <a href="logout.php" style="font-size:18px">Logout?</a>
 </body>
