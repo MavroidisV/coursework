321b7f5a3c0dbd066feda10bc0bb55541aedba86
mavro <mavroidis@hotmail.gr>
2016-03-23 11:19:45 +0000

    coursework

--- regsubmit.php
+++ regsubmit.php
@@ -32,7 +32,7 @@ if(isset($_POST["submit"]))
         if(($query)==1)
         {
             //echo "Thank You! you are now registered.";
-            redirect("location: home.php");
+            redirect("home.php");
         }
 
     }
