86268360d8a1030abc23585f1b359395ca4e72a4
mavro <mavroidis@hotmail.gr>
2016-03-24 07:31:22 +0000

    coursework

--- home.php
+++ home.php
@@ -13,9 +13,8 @@ include("check.php");
 <body>
 <h1 class="hello">Hello, <em><?php echo $login_user;?>!</em></h1>
 
-<a href = bugform.php>submit a form</a><br>
-<a href = mybugs.php>Bugs Submitted</a><br>
-<a href = buglist.php>Full List?</a><br>
+<a href = bugform.php>Bug Report</a><br>
+<a href = buglist.php>Bug List</a><br>
 <a href = comments.php>edo eimaste</a><br>
 <br><br><br>
 <a href="logout.php" style="font-size:18px">Logout?</a>
