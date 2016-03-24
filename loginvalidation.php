0bfe2e034dd7b2e90d5001fe1bcd34bab9338a41
mavro <mavroidis@hotmail.gr>
2016-03-24 12:02:29 +0000

    coursework

--- loginvalidation.php
+++ loginvalidation.php
@@ -2,28 +2,30 @@
 session_start();
 include("connection.php");
 
-if(empty($_POST["username"]) || empty($_POST["password"]))
-{
-    $error=  "Both fields are required.";
-}
-else {
-
+if(isset($_POST["username"]) || isset($_POST["password"])) {
     // Define $username and $password
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // To protect from MySQL injection
+// To protect from MySQL injection
     $username = stripslashes($username);
     $password = stripslashes($password);
     $username = mysqli_real_escape_string($db, $username);
     $password = mysqli_real_escape_string($db, $password);
     $password = md5($password);
 
-    //Check username and password from database
-    $sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
+//Check username and password from database
+    $sql = "SELECT userID FROM users WHERE username='$username' and password='$password'";
 
-    $result=mysqli_query($db,$sql);
+    $result = mysqli_query($db, $sql);
+}
 
+else {
+    $error=  "Both fields are required.";
+}
+
+
+    
 //If username and password exist in our database then create a session.
     //Otherwise echo error.
 if(mysqli_num_rows($result) == 1)
@@ -36,6 +38,6 @@ if(mysqli_num_rows($result) == 1)
 {
     echo "Incorrect username or password.";
 }
-}
+
 
 ?>
