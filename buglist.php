81e9ad4dc77cc650c3f546c0e2bec02ef0dfe4e8
mavro <mavroidis@hotmail.gr>
2016-03-24 16:42:26 +0000

    coursework

--- buglist.php
+++ buglist.php
@@ -16,7 +16,7 @@ $result=mysqli_query($db,$sql);
 
 
 //echo '<h3> </h3>'.$_SESSION["userID"];
-echo '<table border="1" style="width:60%" class="box-table">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<th>'.'Bug ID'.
+echo '<table border="1" style="width:60%" class="box-table" class="input-text">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<th>'.'Bug ID'.
     '</th>'.'<th>'.'Title'.'</th>'.'</table>';
 
 
