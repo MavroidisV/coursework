7ccdf10f37b7501999fb7e572bccd0edde781d4a
mavro <mavroidis@hotmail.gr>
2016-03-24 16:10:15 +0000

    coursework

--- home.php
+++ home.php
@@ -17,7 +17,7 @@ include("check.php");
 
     border-radius: 4px;
     border: 2px #606 solid;
-    width:120px;
+    width:150px;
     background-color: #fff;
     text-align: center;
     margin: auto;/*element middle page*/
