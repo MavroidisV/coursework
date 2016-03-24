5bcfecda1f1be77bd8d7fce9eac995a907dfb1b9
mavro <mavroidis@hotmail.gr>
2016-03-24 13:17:11 +0000

    coursework

--- buglist.php
+++ buglist.php
@@ -30,7 +30,7 @@ while ($row=mysqli_fetch_assoc($result)){
 
 
     echo '<table border="1" style="width:60%">'.'<col width="30">'. '<col width="30">'.'<col width="30">'.'<tr>'.'<td>
-    <a href="mybugs.php?uid=" '.$bugID.'>'.$bugTitle.'</a>'.'<td>'.$bugID.'</td>'.'<td>' . $bugTitle.'</td>'.'<td>'.
+    <a href="mybugs.php?id=" '.$bugID.'>'.$bugTitle.'</a>'.'<td>'.$bugID.'</td>'.'<td>' . $bugTitle.'</td>'.'<td>'.
         "<input type='radio' name='commentRadio' value='$bugID'>".
         '</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
     //echo $bugID;
