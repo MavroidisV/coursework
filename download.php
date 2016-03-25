79f5b505d10deb72e8fc4c586e3b9b710b781037
mavro <mavroidis@hotmail.gr>
2016-03-25 13:33:21 +0000

    coursework

--- download.php
+++ download.php
@@ -1,5 +1,9 @@
-<?php
+<body>
 
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include ('connection.php');
 
 if(isset($_GET['id']))
@@ -27,3 +31,5 @@ if(isset($_GET['id']))
 }
 
 ?>
+
+</body>
