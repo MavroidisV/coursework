441dce128dd8d66cc8e744f7e46ef84601e64a4f
mavro <mavroidis@hotmail.gr>
2016-03-25 18:23:46 +0000

    coursework

--- bugform.php
+++ bugform.php
@@ -13,27 +13,33 @@ include("check.php");
     
 </head>
 <body>
-<h1 class="h1">Bug Form</h1>
 
-<form method="post" action="bugsubmit.php" autocomplete="off" enctype="multipart/form-data" class="box-form">
-
+<form method="post" action="bugsubmit.php" autocomplete="off" enctype="multipart/form-data">
 
 
+    <h1 class="h1">Bug Form</h1>
+    <table cellspacing="10px" class="box-table">
+
+        <tr>
+            <td><label for="title" class="p">Title:</label></td>
+            <td><input type="text" name="title" class="input-text" required></td>
+        </tr>
+
+        <tr>
+            <td class="p">Description:</td>
+            <td><textarea name="description" cols="45" rows="15"  class="input-text1" ></textarea>
+            </td>
+        </tr>
         
-            <label for="title" class="p">Title:</label>
-            <input type="text" name="title" class="input-text" required><br>
+        <tr style="float: right">
+            
+            <td><input name="userfile" type="file" id="userfile" cols="55" rows="15"  class="input-text1">
+            
+            
+        </tr>
 
 
-
-            <label for="description" class="p">Description:</label>
-            <textarea name="description" cols="45" rows="15"  class="input-text1" ><br>
-            
-                
-            
-            <input name="userfile" type="file" id="userfile" cols="55" rows="15"  class="input-text1">
-            
-            
-      
+    </table>
     <br>
     <div style="text-align:center">
         <input type="submit" name="submit" value="Submit your form">
