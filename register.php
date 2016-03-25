a6e4282e9c69f670d79d9f803877036e865cb2ad
mavro <mavroidis@hotmail.gr>
2016-03-25 20:31:21 +0000

    coursework

--- register.php
+++ register.php
@@ -30,22 +30,22 @@ include("regsubmit.php");
         </tr>
         <tr>
         <td><label for="username">Username:</label></td>
-        <td><input type = "text" name="username" class="input-text" required></td>
+        <td><input type = "text" name="username" class="input-text" maxlength="10"required></td>
         </tr>
 
         <tr>
             <td><label for="password">Password:</label></td>
-            <td><input type ="password" name="password" class="input-text" required></td>
+            <td><input type ="password" name="password" class="input-text" minlength="4" maxlength="12" required></td>
         </tr>
 
         <tr>
             <td><label for="email">Email:</label></td>
-            <td><input type ="email" name="email" class="input-text" required><br><?php echo $msg;?></td>
+            <td><input type ="email" name="email" class="input-text" maxlength="15" required><br><?php echo $msg;?></td>
         </tr>
 
         <tr>
             <td><label for="phone">Phone:</label></td>
-            <td><input type="text" name="phone" class="input-text"></td>
+            <td><input type="text" name="phone" class="input-text" maxlength="12"></td>
         </tr>
 
     </table>
